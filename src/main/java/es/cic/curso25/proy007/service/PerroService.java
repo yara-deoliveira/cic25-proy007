c6c1c9dc2e24ecb66847a29516266d1a056e9be4
yara-deoliveira <yaradeoli@gmail.com>
2025-07-17 21:44:48 +0200

    Terminado service de Perro

--- src/main/java/es/cic/curso25/proy007/service/PerroService.java
+++ src/main/java/es/cic/curso25/proy007/service/PerroService.java
@@ -1,5 +1,41 @@
 package es.cic.curso25.proy007.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.cic.curso25.proy007.model.Perro;
+import es.cic.curso25.proy007.repository.PerroRepository;
+
+@Service
 public class PerroService {
+
+    @Autowired
+    private PerroRepository perroRepository;
+
+    public long create(Perro perro) {
+        perroRepository.save(perro);
+
+        return perro.getId();    
+    }
+
+    public Perro get(long id) {
+        Optional<Perro> resultado = perroRepository.findById(id);
+        return resultado.orElse(null);
+    }
+
+    public List<Perro> get() {
+        return perroRepository.findAll();
+    }
+
+    public void update(Perro perro) {
+        perroRepository.save(perro);
+    }
+
+    public void delete(long id) {
+        perroRepository.deleteById(id);
+    }
 
 }
