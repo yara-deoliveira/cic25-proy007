58ab879f623f720010a4527b427186acfed9904a
manuelblancoarenas <no@nomail.no>
2025-07-20 15:23:37 +0200

    Terminadas las 3 capas + algunas correcciones

--- src/main/java/es/cic/curso25/proy007/controller/GatoController.java
+++ src/main/java/es/cic/curso25/proy007/controller/GatoController.java
@@ -1,5 +1,67 @@
 package es.cic.curso25.proy007.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.cic.curso25.proy007.model.Gato;
+import es.cic.curso25.proy007.service.GatoService;
+
+@RestController
+@RequestMapping("/gato")
 public class GatoController {
 
+    static final Logger LOGGER = LoggerFactory.getLogger(GatoController.class);
+    @Autowired
+    private GatoService gatoService;
+    
+    // C
+    @PostMapping
+    public Gato create(@RequestBody Gato gato) {
+        LOGGER.info("Petición para crear el gato " + gato);
+        gato = gatoService.create(gato);
+        return gato;
+    }
+
+    // R (1): 
+    // voy a pasarle un id y devolver el Gato que, guardado en base de datos, tiene dicha id
+    @GetMapping("/{id}") // es decir, los vínculos "/gato/{id}"
+    public Gato get(@PathVariable Long id) {
+        LOGGER.info("Petición para leer datos del gato con id " + id);
+        Gato gato = gatoService.get(id);
+        return gato;
+    }
+
+
+    // R (All)
+    @GetMapping
+    public List<Gato> get() {
+        LOGGER.info("Petición para listar gatos");
+        List<Gato> listaGatos = gatoService.get();
+        return listaGatos;
+    }
+
+    // U
+    @PutMapping
+    public void update(@RequestBody Gato gato) {
+        LOGGER.info("Petición para actualizar el gato " + gato);
+        gatoService.update(gato);
+    }
+
+    // D
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        LOGGER.info("Petición para eliminar el gato de id " + id);
+        gatoService.delete(id);
+    }
 }
--- src/main/java/es/cic/curso25/proy007/service/GatoService.java
+++ src/main/java/es/cic/curso25/proy007/service/GatoService.java
@@ -9,61 +9,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import es.cic.curso25.proy007.model.Gato;
-import es.cic.curso25.proy007.model.Perro;
 import es.cic.curso25.proy007.repository.GatoRepository;
 
 @Service
 public class GatoService {
 
-private static final Logger LOGGER = LoggerFactory.getLogger(GatoService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(GatoService.class);
 
-@Autowired
-private GatoRepository gatoRepository;
+    @Autowired
+    private GatoRepository gatoRepository;
 
-// C
-public Gato create(Gato gato) {
-    LOGGER.info("Guardo gato" + gato);
-    // guardo el gato en base de datos
-    gatoRepository.save(gato);
-    return gato;
-}
-
-// R (1)
-public Gato get(Long id) {
-    // como lo hago con Long y no long, tengo que dividir casos
-    if (id == null) {
-        LOGGER.info("Imposible; no puedo hallar un gato asociado a la id" + id);
-        return null; 
-    }
-    else {
-        LOGGER.info("Obtengo gato de id: " + id);
-        Optional<Gato> gato = gatoRepository.findById(id); // el Optional es por si la id no está en base de datos
-        return gato.orElse(null); // es decir, si es Optional, devuelvo null
-    }
-}
-
-// si hubiese guardado id como long y no Long
-// public Gato get(long id) {
-    // la id nunca puede ser null, luego ahí me ahorro el problema
-    // Optional<Gato> gato = gatoRepository.findById(); // el Optional es por si la id no está en base de datos
-    // return gato.orElse(null); // es decir, si es Optional, devuelvo null
-// }
-
-
-// R (all)
-public List<Gato> get() {
-    return gatoRepository.findAll();
-}
-
-
-// U
-public void update(Gato gato) {
+    // C
+    public Gato create(Gato gato) {
+        LOGGER.info("Guardando el gato " + gato);
+        // guardo el gato en base de datos
         gatoRepository.save(gato);
-}
+        return gato;
+    }
+
+    // R (1)
+    public Gato get(Long id) {
+        // como lo hago con Long y no long, tengo que dividir casos
+        if (id == null) {
+            LOGGER.info("Imposible; no se puede hallar un gato asociado a la id " + id);
+            return null; 
+        }
+        else {
+            LOGGER.info("Se obtiene el gato de id " + id);
+            Optional<Gato> gato = gatoRepository.findById(id); // el Optional es por si la id no está en base de datos
+            return gato.orElse(null); // es decir, si es Optional, devuelvo null
+        }
+    }
+
+    // si hubiese guardado id como long y no Long
+    // public Gato get(long id) {
+        // la id nunca puede ser null, luego ahí me ahorro el problema
+        // Optional<Gato> gato = gatoRepository.findById(); // el Optional es por si la id no está en base de datos
+        // return gato.orElse(null); // es decir, si es Optional, devuelvo null
+    // }
 
 
-// D
-public void delete(Long id) {
-    gatoRepository.deleteById(null);
-}
+    // R (all)
+    public List<Gato> get() {
+        LOGGER.info("Se listan todos los gatos");
+        return gatoRepository.findAll();
+    }
+
+
+    // U
+    public void update(Gato gato) {
+        LOGGER.info("Se actualiza el gato " + gato);
+        gatoRepository.save(gato);
+    }
+
+
+    // D
+    public void delete(Long id) {
+        LOGGER.info("Se elimina el gato de id " + id);
+        gatoRepository.deleteById(id);
+    }
 }
